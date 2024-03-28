0fa3763b351d86de84d27d0b444e0a2335e11d84
PrabakaranTS <prabakaran.ts91@gmail.com>
2024-03-28 17:15:19 +0530

    test oidc

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from "./router/router";
+import store from './store';
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  store,
+  router,
+  vuetify
+}).$mount('#app')
---
+++ src/views/Callback.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="keep">
+    <v-app-bar
+        app
+        clipped-left
+        color="amber"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <span class="title ml-3 mr-5">Store&nbsp;<span class="font-weight-light">Online</span></span>
+    </v-app-bar>
+    <v-main class="grey lighten-4">
+      <v-progress-linear :indeterminate="true" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'Callback',
+  mounted() {
+    this.$store.dispatch("auth/signinRedirectCallback")
+  }
+}
+</script>
